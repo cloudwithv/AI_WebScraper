4842615acb0b72b36e35cfa571cc53d496f00ae3
cloudwithv <vincent.torrijos@cbre.com>
2024-12-13 11:12:19 -0600

    Working without .lower()

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ def test():
 
     for i in data["questions"]:
         question = i["question"]
-        answer = [i["answer"]]
+        answer = i["answer"]
         position = []
 
-        ask = input(f"Question: {question} ")
+        ask = input(f"{question} \n")
 
-        if ask in answer:
-            print("Correct")
+        if not isinstance(answer, list):
+            if ask == answer:
+                print("Correct")
+            if ask != answer:
+                print("Incorrect")
+                print(f"Answer: {answer}")
+
         else:
-            print("Incorrect")
-            print(f"Answer: {answer}")
+            if isinstance(answer, list):
+                if ask in answer:
+                    print("Correct")
+                if ask not in answer:
+                    print("Incorrect")
+                    print(f"Answer: {answer}")
 
 
 
